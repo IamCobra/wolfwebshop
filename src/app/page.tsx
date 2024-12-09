93ea6822be5aed2416d2b2ede58fcead458c5c3c
Ikhvans Mac <ikhvan95@gmail.com>
2024-12-09 16:40:01 +0100

    next image fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import { Navigation, Pagination, Autoplay } from "swiper/modules";
-import Image from "next/image";
+import Image from "next/legacy/image";
 import Typed from "typed.js";
 import { useEffect, useRef, useState } from "react";
 import Link from "next/link";
--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import Image from "next/image";
+import Image from "next/legacy/image";
 import { useState } from "react";
 import { ChevronDown, ChevronUp } from "lucide-react";
 
